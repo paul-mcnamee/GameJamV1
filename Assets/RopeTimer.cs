a4faa0cad1798fe42fab912f5337f57e75812280
Paul McNamee <paulmc121@gmail.com>
2022-04-20 20:20:30 -0600

    add initial commit scaling for the rope

--- Assets/RopeTimer.cs
+++ Assets/RopeTimer.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class RopeTimer : MonoBehaviour
 {
+    public SpriteRenderer Left;
+    public SpriteRenderer Right;
+    public SpriteRenderer Middle;
+
+    private void Awake()
+    {
+        Vector2 initialMiddleSize = Middle.size;
+        Middle.size = new Vector2(0f, initialMiddleSize.y);
+        Middle.transform.position = new Vector2(Left.transform.position.x, Left.transform.position.y);
+    }
+
     // Start is called before the first frame update
     void Start()
     {

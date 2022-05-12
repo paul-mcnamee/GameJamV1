3427bbd6743170b3e48b78faccf8952e46007040
Toomas Allen <thoomasallen8@gmail.com>
2022-05-11 21:58:37 -0700

    procedural generation now working

--- Assets/MazeMinigame/Scripts/MazeGenerator.cs
+++ Assets/MazeMinigame/Scripts/MazeGenerator.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour
 {
+    private GameObject[,] maze;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //generate mase
+        algorithm();
+        //instantiate maze walls
+        maze = new GameObject[height*3,width*3];
+        for(int i = 0; i < height*3; i++)
+        {
+            for (int j = 0; j < width*3; j++)
+            {
+                
+                var obj = Instantiate(wall,
+                    new Vector2(i * wall.GetComponent<Renderer>().bounds.size.x, j * wall.GetComponent<Renderer>().bounds.size.y),
+                    Quaternion.identity);
+                obj.transform.parent = transform;
+                maze[i,j] = obj;
+                
+            }
+        }
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Destroy(maze[i*3+1,j*3+1]);
+                //maze[i*3,j*3].GetComponent<Renderer>().material.color = Color.red;
+                if ((grid[i, j] & N) == N)
+                    Destroy(maze[i * 3 , j * 3 + 1]);
+                if ((grid[i, j] & S) == S)
+                    Destroy(maze[i * 3 + 2, j * 3 + 1]);
+                if ((grid[i, j] & E) == E)
+                    Destroy(maze[i * 3 + 1, j * 3 + 2]);
+                if ((grid[i, j] & W) == W)
+                    Destroy(maze[i * 3 + 1, j * 3]);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -34,17 +68,17 @@ public class MazeGenerator : MonoBehaviour
     public static int width = 10;
     public static int height = 30;
     public int weight = 2;
-    int seed = Random.Range(0, 255);
-
+    
+    public GameObject wall;
 
     //# --------------------------------------------------------------------
     //# 2. Set up constants to aid with describing the passage directions
     //# --------------------------------------------------------------------
 
-    int N = 1;
-    int S = 2;
-    int E = 4;
-    int W = 8;
+    const int N = 1;
+    const int S = 2;
+    const int E = 4;
+    const int W = 8;
     //# --------------------------------------------------------------------
     //# 3. Data structures to assist the algorithm
     //# --------------------------------------------------------------------
@@ -55,65 +89,73 @@ public class MazeGenerator : MonoBehaviour
     //# 4. A simple routine to emit the maze as ASCII
     //# --------------------------------------------------------------------
 
-    private void display_maze(int[,] grid)
-    { }
-}
-//        print("\e[H"); // move to upper-left
-//        print(" " + "_" * (grid[0].length * 2 - 1));
-        
-//        for (int x =0; x < width; x++) {
-//            print("|");
-         
-//            for (int y = 0; y < height; y++) {
-//          if (cell == 0 && y + 1 < grid.length && grid[y + 1][x] == 0)
-//            {
-//                print(" ");
-//            }
-//            else
-//            {
-//                print((cell & S != 0) ? " " : "_");
-//            }
+    //private void display_maze(int[,] grid)
+    //{
+
+    //    Debug.Log("\e[H"); // move to upper-left
+    //    Debug.Log(" " + "_" * (height * 2 - 1));
+
+    //    for (int x = 0; x < width; x++)
+    //    {
+    //        print("|");
+
+    //        for (int y = 0; y < height; y++)
+    //        {
+    //            if (cell == 0 && y + 1 < grid.length && grid[y + 1][x] == 0)
+    //            {
+    //                print(" ");
+    //            }
+    //            else
+    //            {
+    //                print((cell & S != 0) ? " " : "_");
+    //            }
 
 
-//            if (cell == 0 && x + 1 < row.length && row[x + 1] == 0)
-//            {
-//                print((y + 1 < grid.length && (grid[y + 1][x] == 0 || grid[y + 1][x + 1] == 0)) ? " " : "_")
-//                }
-//            else if (cell & E != 0)
-//            {
-//                print(((cell | row[x + 1]) & S != 0) ? " " : "_")}
-//            else
-//            {
-//                print "|"
-//                }
-//        } }
-//    puts
-//}
-//    }
-             //   }
+    //            if (cell == 0 && x + 1 < row.length && row[x + 1] == 0)
+    //            {
+    //                print((y + 1 < grid.length && (grid[y + 1][x] == 0 || grid[y + 1][x + 1] == 0)) ? " " : "_")
+    //                                }
+    //            else if (cell & E != 0)
+    //            {
+    //                print(((cell | row[x + 1]) & S != 0) ? " " : "_")}
+    //            else
+    //            {
+    //                print "|"
+    //                                }
+    //        }
+    //    }
+    //    puts
+    //}
+
+
 
 //# --------------------------------------------------------------------
 //# 5. Sidewinder algorithm
 //# --------------------------------------------------------------------
-//private void algorithm() {
-//    for (int y = 0; y < height; y++){
-//        int run_start = 0;
-//        for (int x = 0; x < width; x++) {
-//            display_maze(grid);
-//            if (y > 0 && (x + 1 == width || Random.Range(0, weight) == 0)) {
-//                int cell = run_start + Random.Range(0, x - run_start + 1);
-//                grid[y, cell] |= N;
-//                grid[y - 1, cell] |= S;
-//                run_start = x + 1; 
-//            }
-//            else if (x + 1 < width) {
-//                grid[y,x] |= E;
-//                grid[y,x + 1] |= W;
-//            }
-//        }
-//    }
-//}
-
+private void algorithm()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            int run_start = 0;
+            for (int x = 0; x < width; x++)
+            {
+                
+                if (y > 0 && (x + 1 == width || Random.Range(0, weight) == 0))
+                {
+                    int cell = run_start + Random.Range(0, x - run_start + 1);
+                    grid[y, cell] |= N;
+                    grid[y - 1, cell] |= S;
+                    run_start = x + 1;
+                }
+                else if (x + 1 < width)
+                {
+                    grid[y, x] |= E;
+                    grid[y, x + 1] |= W;
+                }
+            }
+        }
+    }
+}
 //# --------------------------------------------------------------------
 //# 6. Show the parameters used to build this maze, for repeatability
 //# --------------------------------------------------------------------
